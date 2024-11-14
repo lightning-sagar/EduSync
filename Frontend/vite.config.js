33ed4ce8ccc2e93324d6b357b3646b825d3a889e
Sagar Seth <lightningsagar0@gmail.com>
2024-11-14 14:33:35 +0530

    added the backend link

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: 'http://localhost:5000',
+        target: 'https://edu-sync-eight.vercel.app',
         changeOrigin: true,
         secure: false,
       },
