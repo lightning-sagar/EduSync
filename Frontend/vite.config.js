11c79c203faea5c3b5661d4788ebcd714267fba0
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-11-17 21:40:51 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: 'https://edu-sync-backend-seven.vercel.app',
+        target: 'https://edu-sync-backend-seven.vercel.app/',
         changeOrigin: true,
         secure: false,
       },
