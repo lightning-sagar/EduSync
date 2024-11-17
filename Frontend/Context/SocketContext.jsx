9703b9bd3f06cbdee2e9d87b10183c661b5973ad
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-11-17 15:38:51 +0530

    Update SocketContext.jsx

--- Frontend/Context/SocketContext.jsx
+++ Frontend/Context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider =({children})=>{
     const user = useRecoilValue(userAtom);
     const [onlineUser,setOnlineUser] = useState([])
     useEffect(()=>{
-       const socket = io("https://edu-sync-backend-hecj1wnyj-lightning-sagars-projects.vercel.app/",{
+       const socket = io("https://edu-sync-backend-seven.vercel.app/",{
            query:{
             userId: user?._id,
            }
