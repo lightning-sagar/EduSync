b83f5fa75bf4d7a9ff514cf0c1bca536858e7060
Sagar Seth <lightningsagar0@gmail.com>
2024-11-17 22:47:01 +0530

    pushing

--- Backend/Middleware/protectRoute.js
+++ Backend/Middleware/protectRoute.js
@@ -8,7 +8,7 @@ const protectRoute = async (req, res, next) => {
             console.log('No token provided');
             return res.status(401).json({ error: "Unauthorized: No token provided" });
         }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, "thisismysecrect");
         const user = await User.findById(decoded.userId).select("-password");
         if (!user) {
             return res.status(404).json({ error: "Unauthorized: User not found" });
--- Backend/util/helper/generateCookie.js
+++ Backend/util/helper/generateCookie.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateCookie = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ userId }, "thisismysecrect", {
         expiresIn: '30d'
     });
     res.cookie('jwt', token, {
