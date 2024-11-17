e5504a25e6fbbd3f46d2d062eb88d47d5b2bcec6
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-11-17 21:19:25 +0530

    pushing

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -7,6 +7,14 @@ import User from "../Models/User.js";
 
 const app = express();
 
+app.use(cors(
+    {
+        origin: ["http://localhost:5000", "https://edu-sync-front.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE"],  
+        credentials: true
+    }
+));
+
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
