cb111b0aed2bd74144b20db2b4f1f4b079059ccc
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-11-17 21:18:54 +0530

    pushing

--- Backend/index.js
+++ Backend/index.js
@@ -19,15 +19,6 @@ cloudinary.config({
     timeout: 6000000
 });
 
-app.use(cors(
-    {
-        origin: ["http://localhost:5000", "https://edu-sync-front.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-        credentials: true
-    }
-));
-
-
 connectDB();
 const _dirname = path.resolve();
 app.use(express.json({ limit: "50mb" }));
