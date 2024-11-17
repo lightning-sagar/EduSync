8ba7428a36dbd81e47877bded0c0d41c62fc127c
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-11-17 23:27:01 +0530

    Update socket.js

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -14,7 +14,8 @@ console.log(process.env.JWT_Secret)
 app.use(cors(
     {
         origin: ["http://localhost:5000", "https://edu-sync-front.vercel.app","https://edu-sync-backend-seven.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE"]
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
     }
 ));
 
